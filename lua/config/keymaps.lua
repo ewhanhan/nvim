212d92f6b4fd07c1547c7f4e9553c777b2e4795a
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2025-07-05 01:10:07 -0400

    Add key mappings for moving selected lines in visual mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 local map = vim.keymap.set
 map("n", "<leader>fw", ":w<cr>", { desc = "Write buffer / save file" })
 map("n", "<leader>fW", ":wall<cr>", { desc = "Write all buffer(s) / save all file(s)" })
+-- vscode style shifting
+map("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move selected lines down" })
+map("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move selected lines up" })
