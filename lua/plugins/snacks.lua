e14a4160f2e2f271d33133f224dd84c9a550a0c4
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2025-07-08 17:24:23 -0400

    Improve snacks exclude

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,28 +5,29 @@ return {
       picker = {
         hidden = true,
         ignored = true,
-        exclude = { ".git", "node_modules" },
+        exclude = {
+          -- Version control
+          ".git",
+          -- Dependencies
+          "node_modules",
+          "vendor",
+          -- Build outputs
+          "dist",
+          "build",
+          "out",
+          ".next",
+          "target",
+          -- Caches
+          "__pycache__",
+          ".pytest_cache",
+          ".mypy_cache",
+          ".turbo",
+          -- Test artifacts
+          "coverage",
+        },
         sources = {
-          files = {
-            hidden = true,
-            ignored = true,
-          },
           explorer = {
-            hidden = true,
-            ignored = true,
             auto_close = false,
-          },
-          grep = {
-            hidden = true,
-            ignored = true,
-          },
-          grep_word = {
-            hidden = true,
-            ignored = true,
-          },
-          grep_buffers = {
-            hidden = true,
-            ignored = true,
           },
         },
       },
