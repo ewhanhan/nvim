a9d9e078fc22d56be316dfb8d7216f915733dc30
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2025-07-09 19:45:39 -0400

    Turn off snacks animations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,13 +1,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 vim.g.python3_host_prog = "~/.pyenv/versions/3.12.10/envs/py3nvim/bin/python"
 
+vim.g.snacks_animate = false
+
 local opt = vim.opt
-local o = vim.o
 
 opt.swapfile = false
 opt.incsearch = true
 opt.spell = false
-
-o.incsearch = true -- starts searching as soon as typing, without enter needed
-o.ignorecase = true -- ignore letter case when searching
-o.smartcase = true -- case insentive unless capitals used in searcher
