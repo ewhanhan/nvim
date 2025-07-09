664a9e96649d9c63e56dc99da265d9e2ba6d92c6
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2025-07-09 17:57:16 -0400

    Enhance snacks.nvim configuration by adding source options for files, grep, and explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -26,8 +26,26 @@ return {
           "coverage",
         },
         sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
           explorer = {
+            hidden = true,
+            ignored = true,
             auto_close = false,
+          },
+          grep = {
+            hidden = true,
+            ignored = true,
+          },
+          grep_word = {
+            hidden = true,
+            ignored = true,
+          },
+          grep_buffers = {
+            hidden = true,
+            ignored = true,
           },
         },
       },
