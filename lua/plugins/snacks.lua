05e6eef14f991058340a70792a2797f312738517
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2025-07-25 00:50:25 -0400

    Fix nvim snacks config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,28 +3,6 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
-        hidden = true,
-        ignored = true,
-        exclude = {
-          -- Version control
-          ".git",
-          -- Dependencies
-          "node_modules",
-          "vendor",
-          -- Build outputs
-          "dist",
-          "build",
-          "out",
-          ".next",
-          "target",
-          -- Caches
-          "__pycache__",
-          ".pytest_cache",
-          ".mypy_cache",
-          ".turbo",
-          -- Test artifacts
-          "coverage",
-        },
         sources = {
           files = {
             hidden = true,
@@ -33,19 +11,19 @@ return {
           explorer = {
             hidden = true,
             ignored = true,
-            auto_close = false,
+            auto_close = true,
           },
           grep = {
-            hidden = true,
-            ignored = true,
+            hidden = false,
+            ignored = false,
           },
           grep_word = {
-            hidden = true,
-            ignored = true,
+            hidden = false,
+            ignored = false,
           },
           grep_buffers = {
-            hidden = true,
-            ignored = true,
+            hidden = false,
+            ignored = false,
           },
         },
       },
