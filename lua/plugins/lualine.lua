2fec7681ce76323f8f9abbde040c7a9d1f856812
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2025-07-30 12:30:47 -0400

    Enhance lualine configuration by adding location section with padding

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,14 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
+      opts.sections = opts.sections or {}
+
+      opts.sections.lualine_y = {
+        { "location", padding = { left = 0, right = 1 } },
+      }
+
       opts.sections.lualine_z = {}
+      return opts
     end,
   },
 }
