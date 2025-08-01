53f5d68f702a720b66805043941aa9535dd3e12c
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2025-08-01 10:24:40 -0400

    Update vim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,11 +1,27 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+-- ============================================================================
+-- Global LazyVim Settings
+-- ============================================================================
 -- Python host using dedicated uv environment with neovim module
 vim.g.python3_host_prog = vim.fn.expand("~/.config/nvim-python/bin/python")
 
+-- Performance optimizations
 vim.g.snacks_animate = false
 
+-- Root directory specification
+vim.g.root_spec = { "cwd" }
+
+-- ============================================================================
+-- Editor Options
+-- ============================================================================
 local opt = vim.opt
 
+-- Files and backup
 opt.swapfile = false
+
+-- Search behavior
 opt.incsearch = true
+
+-- Spell checking
 opt.spell = false
