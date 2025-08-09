d8dac79a9843ca7872e719ada0d7a1ddd327a23e
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2025-08-09 17:30:17 -0400

    Update yamll format to use single quote

---
+++ lua/plugins/yaml.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      local yaml_settings = opts.servers.yamlls.settings.yaml
+      yaml_settings.format = vim.tbl_deep_extend("force", yaml_settings.format or {}, {
+        enable = true,
+        singleQuote = true,
+      })
+
+      return opts
+    end,
+  },
+}
