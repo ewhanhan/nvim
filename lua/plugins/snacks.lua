fea7a005f0b2dfd1f1b7cd1e4723be30e5021a10
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2025-09-01 10:36:22 -0400

    Add lazydev and update snacks config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,7 +10,7 @@ return {
           },
           explorer = {
             hidden = true,
-            ignored = true,
+            ignored = false,
             auto_close = true,
           },
         },
