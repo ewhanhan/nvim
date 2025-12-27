b671f890dc36e3a77565e32337a75209d4eb1123
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2025-12-27 16:46:09 -0500

    style(lua): format lazy.lua config tables

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,9 +11,17 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
-      { '\nPress any key to exit...' },
+      {
+        'Failed to clone lazy.nvim:\n',
+        'ErrorMsg',
+      },
+      {
+        out,
+        'WarningMsg',
+      },
+      {
+        '\nPress any key to exit...',
+      },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
@@ -27,13 +35,19 @@ require('lazy').setup({
       'LazyVim/LazyVim',
       import = 'lazyvim.plugins',
     },
-    { import = 'plugins' },
+    {
+      import = 'plugins',
+    },
   },
   defaults = {
     lazy = false,
     version = false,
   },
-  install = { colorscheme = { 'tokyonight', 'gruvbox' } },
+  install = {
+    colorscheme = {
+      'gruvbox',
+    },
+  },
   checker = {
     enabled = true,
     notify = false,
