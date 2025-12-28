12db2f38b9084389242d5b8122209bbb92a219f0
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2025-12-28 18:11:43 -0500

    feat(snacks): add copy_file_path action for picker
    Add custom action to copy file paths in multiple formats:
    - BASENAME, EXTENSION, FILENAME
    - PATH (absolute, cwd-relative, home-relative)
    - URI
    
    Keybindings: Y and <C-y> in list, <C-y> in input mode.
    Works in both picker and explorer sources.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,6 +13,39 @@ return {
     },
     opts = {
       picker = {
+        actions = {
+          copy_file_path = function(_, item, _)
+            if not item then return end
+
+            local file = item.file or item.text
+            if not file then return end
+
+            local vals = {
+              ['BASENAME'] = vim.fn.fnamemodify(file, ':t:r'),
+              ['EXTENSION'] = vim.fn.fnamemodify(file, ':t:e'),
+              ['FILENAME'] = vim.fn.fnamemodify(file, ':t'),
+              ['PATH'] = file,
+              ['PATH (CWD)'] = vim.fn.fnamemodify(file, ':.'),
+              ['PATH (HOME)'] = vim.fn.fnamemodify(file, ':~'),
+              ['URI'] = vim.uri_from_fname(file),
+            }
+
+            local options = vim.tbl_filter(function(val)
+              return vals[val] ~= ''
+            end, vim.tbl_keys(vals))
+            table.sort(options)
+
+            vim.ui.select(options, {
+              prompt = 'Copy to clipboard:',
+              format_item = function(i) return ('%s: %s'):format(i, vals[i]) end,
+            }, function(choice)
+              if choice then
+                vim.fn.setreg('+', vals[choice])
+                Snacks.notify.info('Yanked: ' .. vals[choice])
+              end
+            end)
+          end,
+        },
         sources = {
           files = {
             hidden = true,
@@ -22,7 +55,14 @@ return {
             hidden = true,
             ignored = false,
             auto_close = true,
+            win = {
+              list = { keys = { ['Y'] = 'copy_file_path', ['<C-y>'] = 'copy_file_path' } },
+            },
           },
+        },
+        win = {
+          list = { keys = { ['Y'] = 'copy_file_path', ['<C-y>'] = 'copy_file_path' } },
+          input = { keys = { ['<C-y>'] = { 'copy_file_path', mode = { 'n', 'i' } } } },
         },
       },
     },
