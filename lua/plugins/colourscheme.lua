97eeac04e1a504babe20fa1b78b274687f614a6c
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2026-01-16 13:29:28 -0500

    refactor(colorscheme): add toggle for gruvbox variants
    Use enabled flag to switch between ellisonleao/gruvbox.nvim and
    sainnhe/gruvbox-material without keeping both plugins installed.

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,12 +1,26 @@
-return {
-  -- add gruvbox
-  { 'ellisonleao/gruvbox.nvim' },
+local active_colorscheme = 'gruvbox-material' -- 'gruvbox' or 'gruvbox-material'
 
-  -- Configure LazyVim to load gruvbox
+return {
+  {
+    'ellisonleao/gruvbox.nvim',
+    enabled = active_colorscheme == 'gruvbox',
+  },
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    enabled = active_colorscheme == 'gruvbox-material',
+    config = function()
+      vim.g.gruvbox_material_background = 'medium'
+      vim.g.gruvbox_material_foreground = 'material'
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_better_performance = 1
+    end,
+  },
   {
     'LazyVim/LazyVim',
     opts = {
-      colorscheme = 'gruvbox',
+      colorscheme = active_colorscheme,
     },
   },
 }
