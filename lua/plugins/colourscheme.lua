7206a785fa00ebe794a47f04210f4d7c9d2deadb
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2026-02-22 10:16:11 -0500

    chore(colourscheme): reorganize gruvbox-material config and enable transparency

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -12,10 +12,11 @@ return {
     enabled = active_colorscheme == 'gruvbox-material',
     config = function()
       vim.g.gruvbox_material_background = 'medium'
-      vim.g.gruvbox_material_foreground = 'material'
-      vim.g.gruvbox_material_enable_italic = 1
       vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_enable_italic = 1
       vim.g.gruvbox_material_float_style = 'blend'
+      vim.g.gruvbox_material_foreground = 'material'
+      vim.g.gruvbox_material_transparent_background = 1
     end,
   },
   {
