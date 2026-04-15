efe3620ee7f46eb530afbb514ac0081ecf469286
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2026-04-15 19:28:10 -0400

    chore(yazi): clean up yazi config

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -14,6 +14,7 @@
 return {
   {
     'mikavilpas/yazi.nvim',
+    version = '*',
     enabled = vim.fn.executable('yazi') == 1,
     event = 'VeryLazy',
     dependencies = {
@@ -24,23 +25,11 @@ return {
       { '<leader>E', '<cmd>Yazi cwd<cr>', desc = 'Explorer Yazi (cwd)' },
       { '<leader>fe', '<cmd>Yazi<cr>', desc = 'Explorer Yazi' },
       { '<leader>fE', '<cmd>Yazi cwd<cr>', desc = 'Explorer Yazi (cwd)' },
-      {
-        '<leader>-',
-        mode = { 'n', 'v' },
-        '<cmd>Yazi cwd<cr>',
-        desc = 'Open yazi in cwd',
-      },
-      {
-        '<c-up>',
-        '<cmd>Yazi toggle<cr>',
-        desc = 'Resume last yazi session',
-      },
     },
     init = function()
       -- Prevent netrw from loading since yazi handles directories
       vim.g.loaded_netrwPlugin = 1
     end,
-    ---@type YaziConfig
     opts = {
       open_for_directories = true,
       keymaps = {
